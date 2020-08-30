ad9e613e0db8f4ecf8aadccded26c6988fb629d6
ducnt0706 <51743057+ducnt0706@users.noreply.github.com>
2020-08-30 16:49:18 +0700

    change index

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import AppXX from './src/navigation/MainNavigator';
-//import {LoginScreen} from './src/containers/authen';
+import {LoginScreen} from './src/containers/authen';
 //import {UserProfile} from './src/containers/profile';
-AppRegistry.registerComponent(appName, () => AppXX);
+AppRegistry.registerComponent(appName, () => LoginScreen);
